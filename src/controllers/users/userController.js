f966b08d952d5b157148852522770c993575d52b
newtsarthur <tutuhoficial13@gmail.com>
2025-04-26 19:52:17 -0300

    login

--- src/controllers/users/userController.js
+++ src/controllers/users/userController.js
@@ -53,3 +53,46 @@ export const register = async (req, res) => {
     res.status(500).json({ message: "Erro no servidor.", error: error.message });
   }
 };
+
+//Login
+export const login = async ( req, res ) => {
+  try {
+    const userInfo = req.body;
+
+    //Search user in db
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email }
+    });
+
+    //Check if user exist in db
+    if (!user) {
+      return res.status(404).json({ message: "Esse usuário não existe! "});
+    }
+
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+
+    //Check password saved with typed password
+    if(!isMatch)
+    {
+      return res.status(400).json({ message: "Senha incorreta!" });
+    }
+
+    //Generate Token JWT
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '7d' });
+
+    //Return token and user info
+    res.status(200).json({
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        phone: user.phone,
+        adress: user.adress,
+        createdAt: user.createdAt,
+      },
+    });
+  } catch (error) {
+    console.error( "Erro ao tentar fazer login", error );
+    res.status(500).json({ message: "Erro no servidor.", error: error.message });
+  }
+};
--- src/routes/public.js
+++ src/routes/public.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { register } from '../controllers/users/userController.js';
+import { register, login } from '../controllers/users/userController.js';
 
 const router = express.Router();
 
 // Auth
 router.post('/register', register);
+router.post('/login', login);
+
 
 export default router;
